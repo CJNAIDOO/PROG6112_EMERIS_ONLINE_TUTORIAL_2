edce4cfc95c9698131aea3f402a59ad7fd5300cc
CJNAIDOO <naidoocj2019@gmail.com>
2026-08-26 15:28:27 +0200

    created TrainingSession class, with constructor, getters, setters, abstract startSession(), and overridden toString() methods

---
+++ app/src/main/java/com/example/tutorial2/cam/TrainingSession.java
@@ -0,0 +1,57 @@
+package com.example.tutorial2.cam;
+
+
+public class TrainingSession {
+    
+    // Shared fields for every kind of session
+    private String name;
+    private String trainer;
+    private int duration; // in minutes
+    
+    /*  CONSTRUCTOR
+            - Can use Setters instead of hard coding intialization
+    */
+    public TrainingSession(String name, String trainer, int duration) {
+        this.name = name;
+        this.trainer = trainer;
+        this.duration = duration;
+    }
+ 
+    //  GETTERS
+    public String getName() {
+        return name;
+    }
+ 
+    public String getTrainer() {
+        return trainer;
+    }
+ 
+    public int getDuration() {
+        return duration;
+    }
+    
+    //Setters
+     public void setName(String name) {
+        this.name=name;
+    }
+ 
+    public void setTrainer(String trainer) {
+        this.trainer=trainer;
+    }
+ 
+    public void getDuration(int duration) {
+       this.duration=duration;
+    }
+    
+    /*
+        ABSTRACT METHOD
+            - No body here as every child class MUST override this.
+    */
+    public abstract void startSession();
+ 
+    //Overriden, non-abstract toString method that EVERY session type uses, UNLESS overridden (optional, if child class needs different version of the method)
+    @Override
+    public String toString() {
+        return "Session: " + name + " | Trainer: " + trainer + " | Duration: " + duration + " min";
+    }
+}
